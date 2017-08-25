66961788dc66f51a954ee0cde94af4b43237d8bd
mohd afeef <afeef1915@gmail.com>
2017-08-25 18:34:51 +0530

    fos user fixes

--- src/Acme/StoreBundle/Controller/RestApiController.php
+++ src/Acme/StoreBundle/Controller/RestApiController.php
@@ -17,19 +17,17 @@ use JMS\Serializer\Annotation\ExclusionPolicy;
 use JMS\Serializer\Annotation\Expose;
 use JMS\Serializer\Annotation\Groups;
 use JMS\Serializer\Annotation\VirtualProperty;
-
 use FOS\RestBundle\Controller\FOSRestController;
-use FOS\RestBundle\Controller\Annotations as Rest; 
+use FOS\RestBundle\Controller\Annotations as Rest;
 
 class RestApiController extends FOSRestController {
-    
 
-/**
- * GET Route annotation.
- * @return array
- * @Rest\Get("/api/{version}/all-product")
- * @Rest\View
- */
+    /**
+     * GET Route annotation.
+     * @return array
+     * @Rest\Get("/api/{version}/all-product")
+     * @Rest\View
+     */
     public function showAction() {
         $usersQ = $this->get('doctrine_mongodb')
                 ->getRepository('AcmeStoreBundle:Product')
@@ -40,9 +38,31 @@ class RestApiController extends FOSRestController {
         }
 
 //        return $users;
-       
+
         $view = $this->view($users, Response::HTTP_OK);
         return $view;
+    }
 
+    /**
+     * @Rest\Post("/user/")
+     */
+    public function createAction(Request $request) {
+        $product = new Product();
+        $data = json_decode(file_get_contents('php://input'), true);
+        if(empty($data)) {
+            return new View("NULL VALUES ARE NOT ALLOWED", Response::HTTP_NOT_ACCEPTABLE);
         }
+        $product->setName($data['name']['name']);
+        $product->setPrice($data['name']['price']);
+        // $product->setName($data['name']);
+        //$product->setPrice($data['price']);
+        $dm = $this->get('doctrine_mongodb')->getManager();
+
+        $dm->persist($product);
+        $dm->flush();
+        //die("data saved successfully");
+        return new View("User Added Successfully", Response::HTTP_OK);
+        //return new Response('Created product id ' . $product->getId());
+    }
+
 }
